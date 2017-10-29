e9eee6a97e2cdd80c7b8fd55462af080ed303ca1
nicole18-meet <nicole18-meet>
2017-10-29 19:28:29 +0200

    lab3

---
+++ lab2/lab2.py
@@ -0,0 +1,20 @@
+import turtle
+turtle.speed(10)
+turtle.pensize(10)
+
+a=20
+for i in range(18):
+	turtle.left(a)
+	turtle.forward(120)
+	turtle.left(90)
+
+	turtle.forward(120)
+	turtle.left(90)
+	
+	turtle.forward(120)
+	turtle.left(90)
+	
+	turtle.forward(120)
+	turtle.left(90)	
+	
+turtle.mainloop()
---
+++ lab3/turtleshapes.py
@@ -0,0 +1,76 @@
+import turtle
+turtle.pensize(10)
+#turtle.color("blue")
+
+#a=20
+#for i in range(18):
+#	turtle.left(a)
+#	turtle.forward(120)
+#	turtle.left(90)
+
+#	turtle.forward(120)
+#	turtle.left(90)
+	
+#	turtle.forward(120)
+#	turtle.left(90)
+	
+#	turtle.forward(120)
+#	turtle.left(90)	
+	
+#turtle.mainloop()
+#howmanysides=6
+#sideslength=120
+#howmanysquares=18
+#degree=60
+#angle=20
+
+#for i in range(howmanysquares):
+#	turtle.left(angle)
+#	for i in range(howmanysides):
+#		turtle.forward(sideslength)
+#		turtle.left(degree)
+#turtle.mainloop()		
+
+#for i in range(5):
+#	if i==1:
+#		turtle.color("blue")
+#	if i==2:
+#		turtle.color("green")
+#	if i==3:
+#		turtle.color("yellow")
+#	if i==4:
+#		turtle.color("pink")
+#	if i==5:
+#		turtle.color("red")	
+				
+#	turtle.forward(100)
+#	turtle.right(144)
+
+#turtle.mainloop()	
+
+
+#turtle.right(90)
+#turtle.forward(50)
+#turtle.right(60)
+#turtle.forward(50)
+#turtle.right(60)
+#turtle.forward(50)
+#turtle.right(60)
+#turtle.forward(50)
+#turtle.right(90)
+#turtle.forward(40)
+
+
+#turtle.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
